05025004b969f90629e31fd3b4ee33a8c7c9b4e9
HarshDhimmar2804 <harsh8487861901@gmail.com>
2025-02-22 17:32:09 +0530

    add edit and delete functionality

--- mongo1/index.js
+++ mongo1/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => console.log("Connection Successful..."))
@@ -15,9 +19,62 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
+//index route
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
   res.render("index.ejs", { chats });
+});
+
+//new route
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+//create route
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    createdAt: new Date(),
+  });
+  newChat
+    .save()
+    .then((res) => {
+      console.log("Chat was saved");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
+});
+
+//edit route
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+//update route
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { msg: newMsg },
+    { runValidators: true, new: true }
+  );
+  res.redirect("/chats");
+});
+
+//destroy route
+app.delete("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  console.log(deletedChat);
+  res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
